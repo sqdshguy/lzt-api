e299e0c8799bb134bb2dece9e24d83c40b775831
Alexander Herasymov <drgerasimova8@gmail.com>
2020-08-05 13:56:40 +0300

    ✨ Security patches and updated endpoint!

--- app.js
+++ app.js
@@ -1,54 +1,126 @@
 /*
 
-Lolzteam Unofficial API
-by Mrsasha45op
-MIT License
+  Lolzteam Unofficial API
+  by Mrsasha45op
+  MIT License
 
 */
 
 
 const express = require("express");
+const helmet = require("helmet");
 const puppeteer = require("puppeteer");
+const rateLimit = require("express-rate-limit");
+const middlewares = require("./middlewares");
 const { parse } = require("node-html-parser");
+
 
 const app = express();
 
+// Security fixes
+app.use(helmet());
+
+// Rate limit API
+const limiter = rateLimit({
+  windowMs: 30 * 1000,
+  max: 15,
+});
+
+
 app.get("/", (req, res) => {
-  res.send("За эту страничку мне не платил Ланской");
+  res.send("Coming soon...");
 });
 
+let cachedData;
+let cacheTime;
 
-app.get("/api/latest", async (req, res) => {
-  const postJson = [];
 
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+app.get("/api/latest", limiter, async (req, res) => {
+  // Cache header for browsers
+  res.set('Cache-Control', 'public, max-age=300, s-maxage=600') 
 
-  await page.setJavaScriptEnabled(true);
-
-  await page.goto("https://lolz.guru");
-  await page.waitForNavigation();
-
-  const content = await page.content();
-  await browser.close();
-
-  const parsed = parse(content);
-  const posts = parsed.querySelectorAll(".PreviewTooltip");
-
-  for (const post of posts) {
-    const title = post.querySelector(".title").querySelector("span").rawText;
-    const authorUsername = post.querySelector(".username").firstChild.rawText;
-    const time = post.querySelector(".DateTime").rawText;
-    const commentCount = post.querySelector(".discussionListItem--replyCount").rawText;
-    postJson.push({
-      title,
-      authorUsername,
-      time,
-      commentCount
-    });
+  // Manual cache
+  if (cacheTime && cacheTime > Date.now() - 30 * 1000) {
+    return res.json(cachedData);
   }
-  res.json(postJson);
+
+  const postJson = [];
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.setJavaScriptEnabled(true);
+
+    await page.goto("https://lolz.guru");
+    await page.waitForNavigation();
+
+    const content = await page.content();
+    await browser.close();
+
+
+    const parsed = parse(content);
+    const posts = parsed.querySelectorAll(".PreviewTooltip");
+
+    for (const post of posts) {
+      const title = post.querySelector(".title").querySelector("span").rawText;
+      const category = post.querySelector(".threadNode").rawText;
+      const authorUsername = post.querySelector(".username").firstChild.rawText;
+      const time = post.querySelector(".DateTime").rawText;
+      const commentCount = post.querySelector(".discussionListItem--replyCount").rawText;
+      const styleElem = post.querySelector(".secondRow").querySelector(".posterDate").querySelector(".username").firstChild;
+      const style = styleElem.getAttribute('class');
+      const userRole = getRole(style);
+
+      postJson.push({
+        title,
+        authorUsername,
+        category,
+        time,
+        commentCount,
+        userRole
+      });
+    }
+    // Manual cache
+    cachedData = postJson;
+    cacheTime = Date.now();
+
+    return res.json(postJson);
+  } catch (error) {
+    return next(error);
+  }
 });
 
 
-app.listen(3000, () => console.log("Ланской съел lolzteam"));
+// First version of getting user roles
+function getRole(style) {
+  const roleNum = style.replace("style", "");
+  if (roleNum >= 25 && roleNum < 1000) {
+    return "unik"
+  }
+  switch (roleNum) {
+    case "22":
+      return "market_seller";
+    case "8":
+      return "supreme";
+    case "2":
+      return "user";
+    case "11":
+      return "forum_seller";
+    case "26":
+      return "legend";
+    case "21":
+      return "user";
+    default:
+      return "user";
+  }
+}
+
+// Middlewares
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening for requests on port ${port}`));
+
+
