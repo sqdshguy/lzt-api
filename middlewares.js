e60acba21704e3a0f9688a2672d998f2149d7fd7
Alexander Herasymov <drgerasimova8@gmail.com>
2020-08-05 14:00:57 +0300

    Unused variable

--- middlewares.js
+++ middlewares.js
@@ -5,7 +5,7 @@ function notFound(req, res, next) {
 }
 
 
-function errorHandler(err, req, res, next) {
+function errorHandler(err, req, res) {
   const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
   res.status(statusCode);
   res.json({
